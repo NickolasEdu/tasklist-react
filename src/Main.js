c12d69bab33d9be07c68b7e95837c8d2d733699f
Nickolas Eduardo <nickolasvieira.el@gmail.com>
2022-02-20 08:46:41 -0300

    form section added

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,26 @@
+import styled from "styled-components";
+
+const StyleDefault = styled.div`
+  margin: auto;
+  max-width: 900px;
+`
 
 
-export default function Main() {
+const Main = () => {
+
   return (
-    <>
+    <StyleDefault>
       <h1>Hello World</h1>
-    </>
+      <form>
+        <input type="text"></input>
+        <button>Add</button>
+        
+        <ul>
+          <li>Item 1</li>
+        </ul>
+      </form>
+    </StyleDefault>
   );
 }
+
+export default Main
